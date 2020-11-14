e4b3295a9b18ef5c98839ac007b571ea5ab29022
Med Yassin Azlouk <azlouknour@gmail.com>
2020-11-14 17:20:09 +0100

    Projektdateien hinzufügen.

---
+++ WebWeatherApp/Seed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherApp.Data;
+using WebWeatherApp.Models;
+
+namespace WebWeatherApp
+{
+    public class Seed
+    {
+        private const int value = 1;
+        public static async Task<WeatherData> PopulateWeatherModel(string city)
+        {
+            FetchData fetch = new FetchData();
+            WeatherData weatherData = await fetch.GetAPIResponse(city,value);
+            return weatherData;
+        }
+    }
+}
